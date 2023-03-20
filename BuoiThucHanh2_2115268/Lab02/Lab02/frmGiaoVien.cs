ce89e0d2b335fe669b4f41eb22184574073c1fc5
La O Thi Thanh <111256992+LaThanhhh@users.noreply.github.com>
2023-03-20 21:19:19 +0700

    Add files via upload

---
+++ BuoiThucHanh2_2115268/Lab02/Lab02/frmGiaoVien.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Lab02
+{
+    public partial class frmGiaoVien : Form
+    {
+        public frmGiaoVien()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmGiaoVien_Load(object sender, EventArgs e)
+        {
+            string lienhe = "http://it.dlu.edu.vn/e-learning/Default.aspx";
+            this.linkLienHe.Links.Add(0, lienhe.Length, lienhe);
+            this.cboMaSo.SelectedItem = this.cboMaSo.Items[0];
+        }
+
+        private void btnChon_Click(object sender, EventArgs e)
+        {
+            int i = this.lbDanhSachMH.SelectedItems.Count - 1;
+            while (i>=0)
+            {
+                this.lbMonHocDay.Items.Add(lbDanhSachMH.SelectedItems[i]);
+                this.lbDanhSachMH.Items.Remove(lbDanhSachMH.SelectedItems[i]);
+                i--;
+            }
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            int i = this.lbMonHocDay.SelectedItems.Count - 1;
+            while (i >= 0)
+            {
+                this.lbDanhSachMH.Items.Add(lbDanhSachMH.SelectedItems[i]);
+                this.lbMonHocDay.Items.Remove(lbDanhSachMH.SelectedItems[i]);
+                i--;
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            this.cboMaSo.Text = "";
+            this.txtHoten.Text = "";
+            this.txtMail.Text = "";
+            this.mtxtSoDt.Text = "";
+            this.rdNam.Checked = false;
+
+            for (int i = 0; i < chklbNgoaiNgu.Items.Count - 1; i++)
+                chklbNgoaiNgu.SetItemChecked(i, false);
+            foreach (object ob in this.lbMonHocDay.Items)
+                this.lbDanhSachMH.Items.Add(ob);
+            this.lbMonHocDay.Items.Clear();
+        }
+
+        private void linkLienHe_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string s = e.Link.LinkData.ToString();
+            Process.Start(s);
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            frmTBGiaoVien frm = new frmTBGiaoVien();
+            frm.SetText(GetGiaoVien().ToString());
+            frm.ShowDialog();
+
+        }
+        public GiaoVien GetGiaoVien()
+        {
+            string gt = "Nam";
+            if (rdNu.Checked)
+                gt = "Nữ";
+            GiaoVien gv = new GiaoVien();
+            gv.MaSo = this.cboMaSo.Text;
+            gv.GioiTinh = gt;
+            gv.HoTen = this.txtHoten.Text;
+            gv.NgaySinh = this.dtpNgayDangSinh.Value;
+            gv.Mail = this.txtMail.Text;
+            gv.SoDT = this.mtxtSoDt.Text;
+            string ngoaingu = "";
+            for (int i = 0; i < chklbNgoaiNgu.Items.Count - 1; i++)
+                if (chklbNgoaiNgu.GetItemChecked(i))
+                    ngoaingu += chklbNgoaiNgu.Items[i] + ";";
+            gv.NgoaiNgu = ngoaingu.Split(';');
+            DanhMucMonHoc mh = new DanhMucMonHoc();
+            foreach (object ob in lbMonHocDay.Items)
+                mh.Them(new MonHoc(ob.ToString()));
+            gv.dsMonHoc = mh;
+            return gv;
+        }
+    }
+}
